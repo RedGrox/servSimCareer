31ab1324db2f58ce0be066f68f0393da3c96379f
RedGrox <grossy98@gmail.com>
2020-07-14 19:49:08 +0200

    Added login Request and fixed CreatUser request

--- api/classifiche.js
+++ api/classifiche.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const router = express.Router();
-const fs = require('fs');
+const fs = require("fs");
 let rawdata;
-let http=require('http');
-const bodyParser = require('body-parser');
+let http = require("http");
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -15,61 +15,52 @@ app.use(bodyParser.json());
     "arg"=[1,25,3]
   }
 */
-router.get('/',(req,res,next)=>{
-    let classifiche=getClassifiche();
-    let bodyReq=req.body;
-    let trueCounter=false;
-    let response=[];
-    if(bodyReq.arg.length==0) //send back all users
-    {
-      res.status(200).json(classifiche);
-    }else
-    {
-      for(let i=0;i<bodyReq.arg.length;i++)
-      {
-        let index=existId(bodyReq.arg[i],classifiche);
-        if(index!=-1)
-        {
-          response.push(classifiche[index]);
-          trueCounter=true;
-        }else {
-          response.push({});
-        }
-      }
-      if(trueCounter)
-      {
-        res.status(200).json(response);
-      }else {
-        res.status(400).json(response);
+router.get("/", (req, res, next) => {
+  let classifiche = getClassifiche();
+  let bodyReq = req.body;
+  let trueCounter = false;
+  let response = [];
+  if (bodyReq.arg.length == 0) {
+    //send back all users
+    res.status(200).json(classifiche);
+  } else {
+    for (let i = 0; i < bodyReq.arg.length; i++) {
+      let index = existId(bodyReq.arg[i], classifiche);
+      if (index != -1) {
+        response.push(classifiche[index]);
+        trueCounter = true;
+      } else {
+        response.push({});
       }
     }
+    if (trueCounter) {
+      res.status(200).json(response);
+    } else {
+      res.status(400).json(response);
+    }
+  }
 });
 
-function getClassifiche()
-{
-  rawdata=fs.readFileSync('DB/classifiche.json');
+function getClassifiche() {
+  rawdata = fs.readFileSync("DB/classifiche.json");
   let classifiche;
-  if(rawdata.length==0)
-  {
-    classifiche=[];
-  }else{
-    classifiche=JSON.parse(rawdata);
+  if (rawdata.length == 0) {
+    classifiche = [];
+  } else {
+    classifiche = JSON.parse(rawdata);
   }
   return classifiche;
 }
-function existId(id,classifiche){
-    let index=-1;
-    id+="";
-    if(classifiche.length!=0)
-    {
-      for(let i=0;i<classifiche.length;i++)
-      {
-        if(id.localeCompare(classifiche[i].id)===0)
-        {
-          index=i;
-        }
+function existId(id, classifiche) {
+  let index = -1;
+  id += "";
+  if (classifiche.length != 0) {
+    for (let i = 0; i < classifiche.length; i++) {
+      if (id.localeCompare(classifiche[i].id) === 0) {
+        index = i;
       }
     }
-    return index;
   }
-module.exports=router;
+  return index;
+}
+module.exports = router;
--- api/utenti.js
+++ api/utenti.js
@@ -1,13 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const router = express.Router();
-const fs = require('fs');
+const fs = require("fs");
+const crypto = require("crypto");
+const secret = "password";
 let rawdata;
-let http=require('http');
-const bodyParser = require('body-parser');
+let http = require("http");
+const bodyParser = require("body-parser");
+let lastId = getLastId();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+router.post("/login", (req, res, next) => {
+  let utenti = getUtenti();
+  let bodyReq = req.body;
+  if (bodyReq.mail) {
+    let mail = bodyReq.mail;
+    let index = existMail(mail, utenti);
+    if (index != -1) {
+      let hashedPsw = crypto
+        .createHmac("sha256", secret)
+        .update(bodyReq.password)
+        .digest("hex");
+      if (hashedPsw.localeCompare(utenti[index].password) === 0) {
+        res.status(200).json(utenti[index]);
+      } else {
+        res.status(400).json({});
+      }
+    } else {
+      res.status(400).json({});
+    }
+  }
+});
 //get users
 /*
   request example:
@@ -16,54 +40,48 @@ app.use(bodyParser.json());
     "arg"=[1,25,3]
   }
 */
-router.get('/',(req,res,next)=>{
-    let utenti=getUtenti();
-    let bodyReq=req.body;
-    let trueCounter=false;
-    let response=[];
-    if(bodyReq.length==0) //send back all users
-    {
-      res.status(200).json(utenti);
-    }else if(bodyReq.mailOrId==1) //send back users with those mail
-    {
-      for(let i=0;i<bodyReq.arg.length;i++)
-      {
-        let index=existMail(bodyReq.arg[i],utenti);
-        if(index!=-1)
-        {
-          response.push(utenti[index]);
-          trueCounter=true;
-        }else {
-          response.push({});
-        }
-      }
-      if(trueCounter)
-      {
-        res.status(200).json(response);
-      }else {
-        res.status(400).json(response);
-      }
-    }else if(bodyReq.mailOrId==0) //send back users with those id
-    {
-      for(let i=0;i<bodyReq.arg.length;i++)
-      {
-        let index=existIdUt(bodyReq.arg[i],utenti);
-        if(index!=-1)
-        {
-          response.push(utenti[index]);
-          trueCounter=true;
-        }else {
-          response.push({});
-        }
-      }
-      if(trueCounter)
-      {
-        res.status(200).json(response);
-      }else {
-        res.status(400).json(response);
+router.get("/", (req, res, next) => {
+  let utenti = getUtenti();
+  let bodyReq = req.body;
+  let trueCounter = false;
+  let response = [];
+  if (bodyReq.length == 0) {
+    //send back all users
+    res.status(200).json(utenti);
+  } else if (bodyReq.mailOrId == 1) {
+    //send back users with those mail
+    for (let i = 0; i < bodyReq.arg.length; i++) {
+      let index = existMail(bodyReq.arg[i], utenti);
+      if (index != -1) {
+        response.push(utenti[index]);
+        trueCounter = true;
+      } else {
+        response.push({});
       }
     }
-  });
+    if (trueCounter) {
+      res.status(200).json(response);
+    } else {
+      res.status(400).json(response);
+    }
+  } else if (bodyReq.mailOrId == 0) {
+    //send back users with those id
+    for (let i = 0; i < bodyReq.arg.length; i++) {
+      let index = existIdUt(bodyReq.arg[i], utenti);
+      if (index != -1) {
+        response.push(utenti[index]);
+        trueCounter = true;
+      } else {
+        response.push({});
+      }
+    }
+    if (trueCounter) {
+      res.status(200).json(response);
+    } else {
+      res.status(400).json(response);
+    }
+  }
+});
 //insert a user
 /*
   post example:
@@ -82,26 +100,35 @@ router.get('/',(req,res,next)=>{
     }
   ]
 */
-router.post('/',(req,res,next)=>{
-  let utenti=getUtenti();
-  let bodyReq=req.body;
-  let trueCounter=false;
-  let arrStatus=[];
+router.post("/", (req, res, next) => {
+  let utenti = getUtenti();
+  let bodyReq = req.body;
+  let trueCounter = false;
+  let arrStatus = [];
   for (let i = 0; i < bodyReq.length; i++) {
-    if(existIdUt(bodyReq[i].id,utenti)==-1&&existMail(bodyReq[i].mail,utenti)==-1)
-    {
+    if (existMail(bodyReq[i].mail, utenti) == -1) {
+      let hashedPsw = crypto
+        .createHmac("sha256", secret)
+        .update(bodyReq[i].password)
+        .digest("hex");
+      bodyReq[i].password = hashedPsw;
+      lastId += 1;
+      bodyReq[i].id = lastId;
       utenti.push(bodyReq[i]);
-      fs.writeFileSync('DB/utenti.json',JSON.stringify(utenti,null,2),'utf8');
-      trueCounter=true;
+      fs.writeFileSync(
+        "DB/utenti.json",
+        JSON.stringify(utenti, null, 2),
+        "utf8"
+      );
+      trueCounter = true;
       arrStatus.push(true);
-    }else{
+    } else {
       arrStatus.push(false);
-    }    
+    }
   }
-  if(trueCounter==true)
-  {
+  if (trueCounter == true) {
     res.status(200).json(arrStatus);
-  }else{
+  } else {
     res.status(400).json(arrStatus);
   }
 });
@@ -112,48 +139,51 @@ router.post('/',(req,res,next)=>{
     "arg"=[1,25,3]
   }
 */
-router.delete('/',(req,res,next)=>{
-  let utenti=getUtenti();
-  let bodyReq=req.body;
-  let trueCounter=false;
-  let arrStatus=[];
-  if(bodyReq.mailOrId==0)
-  {
+router.delete("/", (req, res, next) => {
+  let utenti = getUtenti();
+  let bodyReq = req.body;
+  let trueCounter = false;
+  let arrStatus = [];
+  if (bodyReq.mailOrId == 0) {
     for (let i = 0; i < bodyReq.arg.length; i++) {
-      let index=existMail(bodyReq.arg[i],utenti);
-      if(index!=-1)
-      {
-        utenti.splice(index,1);
-        trueCounter=true;
-        arrStatus[i]=true;
-      }else{
-        arrStatus[i]=false;
-      }      
+      let index = existMail(bodyReq.arg[i], utenti);
+      if (index != -1) {
+        utenti.splice(index, 1);
+        trueCounter = true;
+        arrStatus[i] = true;
+      } else {
+        arrStatus[i] = false;
+      }
     }
-    if(trueCounter)
-    {
-      fs.writeFileSync('DB/utenti.json',JSON.stringify(utenti,null,2),'utf8');
+    if (trueCounter) {
+      fs.writeFileSync(
+        "DB/utenti.json",
+        JSON.stringify(utenti, null, 2),
+        "utf8"
+      );
       res.status(200).json(arrStatus);
-    }else{
+    } else {
       res.status(400).json(arrStatus);
     }
-  }else{
+  } else {
     for (let i = 0; i < bodyReq.arg.length; i++) {
-      let index=existIdUt(bodyReq.arg[i],utenti);
-      if(index!=-1)
-      {
-        utenti.splice(index,1);
-        trueCounter=true;
-        arrStatus[i]=true;
-      }else{
-        arrStatus[i]=false;
-      }      
+      let index = existIdUt(bodyReq.arg[i], utenti);
+      if (index != -1) {
+        utenti.splice(index, 1);
+        trueCounter = true;
+        arrStatus[i] = true;
+      } else {
+        arrStatus[i] = false;
+      }
     }
-    if(trueCounter)
-    {
-      fs.writeFileSync('DB/utenti.json',JSON.stringify(utenti,null,2),'utf8');
+    if (trueCounter) {
+      fs.writeFileSync(
+        "DB/utenti.json",
+        JSON.stringify(utenti, null, 2),
+        "utf8"
+      );
       res.status(200).json(arrStatus);
-    }else{
+    } else {
       res.status(400).json(arrStatus);
     }
   }
@@ -175,68 +205,62 @@ router.delete('/',(req,res,next)=>{
   "autoPreferita":""
   }
 */
-router.put('/',(req,res,next)=>{
-  let utenti=getUtenti();
-  let bodyReq=req.body;
-  let indexId=existIdUt(bodyReq.id,utenti);
-  let indexM=existMail(bodyReq.mail,utenti);
-  if(indexId!=-1&&(indexId==indexM||indexM==-1))
-  {
-    utenti[indexId]=bodyReq;
-    fs.writeFileSync('DB/utenti.json',JSON.stringify(utenti,null,2),'utf8');
+router.put("/", (req, res, next) => {
+  let utenti = getUtenti();
+  let bodyReq = req.body;
+  let indexId = existIdUt(bodyReq.id, utenti);
+  let indexM = existMail(bodyReq.mail, utenti);
+  if (indexId != -1 && (indexId == indexM || indexM == -1)) {
+    utenti[indexId] = bodyReq;
+    fs.writeFileSync("DB/utenti.json", JSON.stringify(utenti, null, 2), "utf8");
     res.status(200).json({});
-  }else{
-    if(indexId==-1)
-    {
-      res.status(400).json({"error":0});//error id
-    }else{
-      res.status(400).json({"error":1});//error mail
+  } else {
+    if (indexId == -1) {
+      res.status(400).json({ error: 0 }); //error id
+    } else {
+      res.status(400).json({ error: 1 }); //error mail
     }
   }
 });
 
-function getUtenti()
-{
-  rawdata=fs.readFileSync('DB/utenti.json');
+function getUtenti() {
+  rawdata = fs.readFileSync("DB/utenti.json");
   let utenti;
-  if(rawdata.length==0)
-  {
-    utenti=[];
-  }else{
-    utenti=JSON.parse(rawdata);
+  if (rawdata.length == 0) {
+    utenti = [];
+  } else {
+    utenti = JSON.parse(rawdata);
   }
   return utenti;
 }
-function existMail(utMail,utenti){
-  let index=-1;
-  if(utenti.length!=0)
-  {
-    for(let i=0;i<utenti.length;i++)
-    {
-      if(utMail.localeCompare(utenti[i].mail)===0)
-      {
-        index=i;
+function existMail(utMail, utenti) {
+  let index = -1;
+  if (utenti.length != 0) {
+    for (let i = 0; i < utenti.length; i++) {
+      if (utMail.localeCompare(utenti[i].mail) === 0) {
+        index = i;
       }
     }
   }
   return index;
 }
 
-function existIdUt(idUt,utenti){
-  let index=-1;
-  idUt+="";
-  if(utenti.length!=0)
-  {
-    for(let i=0;i<utenti.length;i++)
-    {
-      if(idUt.localeCompare(utenti[i].id)===0)
-      {
-        index=i;
+function existIdUt(idUt, utenti) {
+  let index = -1;
+  idUt += "";
+  if (utenti.length != 0) {
+    for (let i = 0; i < utenti.length; i++) {
+      if (idUt.localeCompare(utenti[i].id) === 0) {
+        index = i;
       }
     }
   }
   return index;
 }
 
+function getLastId() {
+  let utenti = getUtenti();
+  return Number(utenti[utenti.length - 1].id);
+}
 
-module.exports=router;
+module.exports = router;
